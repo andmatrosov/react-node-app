cf18cc979fddf1164fdcb34c81a23f41644289ad
andmatrosov <and.matrosow@ya.ru>
2025-06-06 01:25:22 +0400

    Add router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getViewIdeaRoute, getAllIdeasRoute, viewRouteParams } from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute(viewRouteParams)} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
---
+++ webapp/src/lib/routes.ts
@@ -0,0 +1,9 @@
+const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
+  return Object.keys(object).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
+}
+
+export const getAllIdeasRoute = () => '/'
+
+export const viewRouteParams = getRouteParams({ ideaNick: true })
+export type ViewIdeaRouteParams = typeof viewRouteParams
+export const getViewIdeaRoute = ({ ideaNick }: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
