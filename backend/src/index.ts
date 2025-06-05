efb5e81238f1ef61e2f3c5164151c564da9df47c
andmatrosov <and.matrosow@ya.ru>
2025-06-05 12:40:06 +0400

    init trpc on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,20 @@
-console.log('Hello')
-console.log('Priv')
+import express from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
+
+const expressApp = express()
+
+expressApp.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+)
+
+expressApp.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
